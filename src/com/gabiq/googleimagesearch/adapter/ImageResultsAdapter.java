aef55e6a03487302b1ea6c0d6d864a5647603a42
Gabi Zuniga <gabiz@alignd.io>
2014-09-24 15:42:34 -0700

    Added ViewHolder and fixed layout

--- src/com/gabiq/googleimagesearch/adapter/ImageResultsAdapter.java
+++ src/com/gabiq/googleimagesearch/adapter/ImageResultsAdapter.java
@@ -24,15 +24,28 @@ public class ImageResultsAdapter extends ArrayAdapter<ImageResult> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageResult imageInfo = getItem(position);
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_image_result, parent, false);
-        }
-        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        ivImage.setImageResource(0);
-        tvTitle.setText(Html.fromHtml(imageInfo.title));
         
-        Picasso.with(getContext()).load(imageInfo.thumbUrl).into(ivImage);
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_image_result, parent, false);
+
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.ivImage.setImageResource(0);
+        viewHolder.tvTitle.setText(Html.fromHtml(imageInfo.title));
+        
+        Picasso.with(getContext()).load(imageInfo.thumbUrl).into(viewHolder.ivImage);
         return convertView;
+    }
+    
+    private static class ViewHolder {
+        ImageView ivImage;
+        TextView tvTitle;
     }
 }
